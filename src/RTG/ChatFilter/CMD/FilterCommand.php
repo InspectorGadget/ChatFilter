fd737d1ae32cbfdbbb5e7a66bfc38a4f5de65128
InspectorGadget <RTGDaCoder@users.noreply.github.com>
2017-02-03 21:02:32 +0800

    Added Commands | IN DEV

---
+++ src/RTG/ChatFilter/CMD/FilterCommand.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace RTG\ChatFilter\CMD;
+
+/* Essentials */
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+use RTG\ChatFilter\Loader;
+
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+
+class FilterCommand implements CommandExecutor {
+	
+	public $plugin;
+	
+	public function __construct(Loader $plugin) {
+		$this->plugin = $plugin;
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $param) {
+		switch(strtolower($cmd->getName())) {
+			
+			case "cf":
+			
+				if($sender->hasPermission("chatfilter.command")) {
+					
+					if(isset($param[0])) {
+						
+						switch(strtolower($param[0])) {
+							
+							case "help":
+							
+								$sender->sendMessage("[ChatFilter] /cf add [name]");
+								$sender->sendMessage("[ChatFilter] /cf rm [name]");
+							
+								return true;
+							break;
+							
+							case "add":
+								
+								if(isset($param[1])) {
+									
+									$n = $param[1];
+									
+										if(isset($this->plugin->whitelist[strtolower($n)])) {
+											
+											$sender->sendMessage("[ChatFilter] The word '$n' Exists!");
+											
+										}
+										else {
+											
+											$this->plugin->whitelist[strtolower($n)] = $n;
+											$sender->sendMessage("[ChatFilter] You have successfully added $n to the ban list!");
+											$this->plugin->onSave();
+											
+										}
+									
+								}
+								else {
+									$sender->sendMessage("[ChatFilter] /cf help");
+								}
+							
+								return true;
+							break;
+								
+						}
+							
+					}
+					else {
+						$sender->sendMessage("[ChatFilter] /cf help");
+					}
+
+				}
+				else {
+					$sender->sendMessage(TF::RED . "You have no permission to use this command!");
+				}
+			
+				return true;
+			break;
+			
+		}
+		
+	}
+		
+}
