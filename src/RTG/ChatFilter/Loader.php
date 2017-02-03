a59deb4b0ab6d834d15be0a057d5f6b75eb275cc
InspectorGadget <RTGDaCoder@users.noreply.github.com>
2017-02-03 15:31:08 +0800

    Initial Commit

---
+++ src/RTG/ChatFilter/Loader.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace RTG\ChatFilter;
+
+/* Essentials */
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+/* Execution */
+use pocketmine\command\CommandExecutor;
+
+class Loader extends PluginBase implements Listener {
+	
+	public $whitelist;
+	
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		
+		/* For Commands */
+		$this->getCommand("cf")->setExecutor(new FilterCommand ($this));
+		
+		/* CFG Setup */
+		
+		$this->whitelist = array();
+		
+		@mkdir ($this->getDataFolder());
+		$list = new Config($this->getDataFolder() . "bannednames.yml", Config::YAML);
+		$this->whitelist = $list->getAll(true);
+		
+		$this->getLogger()->info("[LEET] ChatFilter has been enabled!");
+	}
+	
+	public function onSave() {
+		$list = new Config($this->getDataFolder() . "bannednames.yml", Config::YAML);
+		$list->setAll($this->whitelist);
+		$list->save():
+	}
+	
+	public function onDisable() {
+		
+	}
+	
+}
